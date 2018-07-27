6f7c6f3e28a8b303c16397d2e760d645e26332df
huang <huangzengmeng@hongte.info>
2018-07-27 15:27:25 +0800

    Initial commit

---
+++ eureka/src/main/java/com/hzm/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.hzm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author huangzengmeng
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+		System.err.println("EurekaApplication 服务启动成功 @_@");
+	}
+}
